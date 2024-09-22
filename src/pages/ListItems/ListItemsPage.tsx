23151f02805c27471433e27b79b21e5b482ebd93
Samuel <smltrs0@gmail.com>
2024-09-21 22:20:36 -0500

    feat: add loading indicator to ListItemsPage

--- src/pages/ListItems/ListItemsPage.tsx
+++ src/pages/ListItems/ListItemsPage.tsx
@@ -1,38 +1,49 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ListItemsContainer from '../../components/ListItemsContainer/ListItemsContainer';
-import './ListItemsPage.css';
-import SearchBar from '../../components/SearchBar/SearchBar';
-import { useDispatch, useSelector } from 'react-redux';
-import { AppDispatch, RootState } from '../../store'; // Adjust the import path according to your project structure
-import { useEffect } from 'react';
-import { fetchProducts } from '../../application/productsThunks';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonButton,
+  IonLoading,
+} from "@ionic/react";
+import ListItemsContainer from "../../components/ListItemsContainer/ListItemsContainer";
+import "./ListItemsPage.css";
+import SearchBar from "../../components/SearchBar/SearchBar";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "../../store"; // Ajusta la ruta de importación según la estructura de tu proyecto
+import { useEffect } from "react";
+import { fetchProducts } from "../../application/productsThunks";
 
 const ListItemsPage: React.FC = () => {
-
   const dispatch = useDispatch<AppDispatch>();
 
   useEffect(() => {
     dispatch(fetchProducts());
   }, [dispatch]);
 
+  const loading = useSelector((state: RootState) => state.products.loading);
+  // const loading = true; // Cambia esta línea por la siguiente
+
   const products = useSelector((state: RootState) => state.products.products);
 
   return (
     <IonPage>
-      {/* <IonHeader>
-        <IonToolbar>
-          <IonTitle>Listado de productos</IonTitle>
-        </IonToolbar>
-      </IonHeader> */}
       <IonContent fullscreen>
-      <SearchBar />
-
+        <SearchBar />
         <IonHeader collapse="condense">
           <IonToolbar>
             <IonTitle size="large">Items</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ListItemsContainer products={products} />
+        {loading && (
+          <IonLoading
+            isOpen={loading}
+            onDidDismiss={() =>{}}
+            message={'Cargando...'}
+          />
+        )}
+        {products && <ListItemsContainer products={products} />}
       </IonContent>
     </IonPage>
   );
